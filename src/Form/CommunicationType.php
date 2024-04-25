69ca86c39568986ff8fd9fb1ca54f0e8dcd7515b
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-04-25 12:16:18 +0200

    Added CommunicationType

---
+++ src/Form/CommunicationType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Communication;
+use App\Entity\Seller;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommunicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateTimeType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('comment', TextareaType::class)
+            ->add('seller', EntityType::class, [
+                'class' => Seller::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Communication::class,
+        ]);
+    }
+}
